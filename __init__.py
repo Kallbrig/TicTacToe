fde7d2c5de721c7abf21b088e4b34bf628007b78
Kristopher Allbright <kallbright@troy.edu>
2020-02-23 21:32:11 -0600

    Added switch turn function

--- __init__.py
+++ __init__.py
@@ -7,7 +7,7 @@ def print_board(board):
         print()
 
 
-def select_play(board):
+def select_play(board, turn):
     if board is None:
         board = [[' ', '|', ' ', '|', ' '],
                  ['-', '+', '-', '+', '-'],
@@ -22,40 +22,47 @@ def select_play(board):
     while not (0 < int(key) < 10):
         key = input("that's not a valid selection.\nPlease select a number 1-9\n")
 
-    select_play(play(int(key), board))
+    select_play(play(int(key), board, turn), switch_turn(turn))
+
+
+def switch_turn(turn):
+    if turn == 'X':
+        return 'O'
+    elif turn == 'O':
+        return 'X'
 
 
 # Switch Statement replacement
-def play(position, board):
+def play(position, board, turn):
     if position == 1:
-        board[0][0] = 'X'
+        board[0][0] = turn
         return board
     elif position == 2:
-        board[0][2] = 'X'
+        board[0][2] = turn
         return board
     elif position == 3:
-        board[0][4] = 'X'
+        board[0][4] = turn
         return board
     elif position == 4:
-        board[2][0] = 'X'
+        board[2][0] = turn
         return board
     elif position == 5:
-        board[2][2] = 'X'
+        board[2][2] = turn
         return board
     elif position == 6:
-        board[2][4] = 'X'
+        board[2][4] = turn
         return board
     elif position == 7:
-        board[4][0] = 'X'
+        board[4][0] = turn
         return board
     elif position == 8:
-        board[4][2] = 'X'
+        board[4][2] = turn
         return board
     elif position == 9:
-        board[4][4] = 'X'
+        board[4][4] = turn
         return board
     else:
-        return "Hello"
+        return "You shouldn't be seeing this you stupid bitch"
 
 
-select_play(None)
+select_play(None, 'X')
