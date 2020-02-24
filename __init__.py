7d4317ce85a3db0f9d7383d63cfd096be5e43268
Kristopher Allbright <kallbright@troy.edu>
2020-02-24 13:24:52 -0600

    Detects a win on any row

--- __init__.py
+++ __init__.py
@@ -55,6 +55,13 @@ def play(position, board, turn):
 # returns nothing. recursive.
 def select_play(board, turn):
     if board is None:
+        print('The selections are as follows:')
+        print_board([['1', '|', '2', '|', '3'],
+                     ['-', '+', '-', '+', '-'],
+                     ['4', '|', '5', '|', '6'],
+                     ['-', '+', '-', '+', '-'],
+                     ['7', '|', '8', '|', '9']])
+        print('\n')
         board = [[' ', '|', ' ', '|', ' '],
                  ['-', '+', '-', '+', '-'],
                  [' ', '|', ' ', '|', ' '],
@@ -64,9 +71,9 @@ def select_play(board, turn):
 
     key = input("Where would you like to play 1-9?\n")
     while not key.isnumeric():
-        key = input("that's not a valid selection.\nPlease select a number 1-9\n")
+        key = input("That is not a valid selection.\nPlease select a number 1-9\n")
     while not (0 < int(key) < 10):
-        key = input("that's not a valid selection.\nPlease select a number 1-9\n")
+        key = input("That is not a valid selection.\nPlease select a number 1-9\n")
     new_board = play(int(key), board, turn)
     new_turn = switch_turn(turn)
     win_checker(new_board)
@@ -79,10 +86,28 @@ def select_play(board, turn):
 def win_checker(board):
     count = 0
     for row in board:
-        for cha in row:
-            if cha == 'X' or cha == 'O':
+        # Number of X's and O's in a row
+        row_countX = 0
+        row_countO = 0
+        for char in row:
+
+            # Steps to perform if character is an X
+            if char == 'X':
                 count = count + 1
+                row_countX = row_countX + 1
+                if row_countX == 3:
+                    print('Game Over! X Wins')
+                    sys.exit()
+            # Steps to perform if character is an O
+            if char == 'O':
+                count = count + 1
+                row_countO = row_countO + 1
+                if row_countO == 3:
+                    print('Game Over! O Wins')
+                    sys.exit()
+    # Checks if board has 9 X+O's
     if count == 9:
+        print_board(board)
         print('Game Over! Congratulations!')
         sys.exit()
 
