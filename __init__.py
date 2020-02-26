e93d1b54c25a9ecdea8cbe8841dacc638ab5f729
Kristopher Allbright <Kallbright@troy.edu>
2020-02-26 11:24:25 -0600

    Added full win checking
    All wins are detected correctly.

--- __init__.py
+++ __init__.py
@@ -23,10 +23,10 @@ def play(position, board, turn):
     if position == 1 and board[0][0] == ' ':
         board[0][0] = turn
         return board
-    elif position == 2  and board[0][2] == ' ':
+    elif position == 2 and board[0][2] == ' ':
         board[0][2] = turn
         return board
-    elif position == 3  and board[0][4] == ' ':
+    elif position == 3 and board[0][4] == ' ':
         board[0][4] = turn
         return board
     elif position == 4 and board[2][0] == ' ':
@@ -70,7 +70,7 @@ def select_play(board, turn):
                  [' ', '|', ' ', '|', ' ']]
     print_board(board)
 
-    key = input("Where would you like to play 1-9?\n")
+    key = input("It's " + turn + "'s move. Where would you like to play 1-9?\n")
     while not key.isnumeric():
         key = input("That is not a valid selection.\nPlease select a number 1-9\n")
     while not (0 < int(key) < 10):
@@ -85,12 +85,56 @@ def select_play(board, turn):
 # ends game when 9 X's or O's are on the board.
 # TO-DO: check if a player has won and notify them.
 def win_checker(board):
-    #     for i in [0, 1, 2, ]:
-    #         for row in board:
-    #             if (row[i] == ('X' or 'O')):
-    #                 print("hi")
-    #
+    # DIAGONAL WIN CHECKER
+    # detects a win on diagonal 1,5,9
+    if board[0][0] == 'X' and board[2][2] == 'X' and board[4][4] == 'X':
+        print_board(board)
+        print("Game over! X wins!")
+        sys.exit()
+    if board[0][0] == 'O' and board[2][2] == 'O' and board[4][4] == 'O':
+        print_board(board)
+        print("Game over! O wins!")
+        sys.exit()
+    # detects a win on diagonal 3,5,7
+    if board[0][4] == 'O' and board[2][2] == 'O' and board[4][0] == 'O':
+        print_board(board)
+        print("Game over! O wins!")
+        sys.exit()
+    if board[0][4] == 'X' and board[2][2] == 'X' and board[4][0] == 'X':
+        print_board(board)
+        print("Game over! X wins!")
+        sys.exit()
 
+    # HORIZONTAL WIN CHECKER
+    # detects a win on vertical 1,4,7
+    if board[0][0] == 'X' and board[2][0] == 'X' and board[4][0] == 'X':
+        print_board(board)
+        print("Game over! X wins!")
+        sys.exit()
+    if board[0][0] == 'O' and board[2][0] == 'O' and board[4][0] == 'O':
+        print_board(board)
+        print("Game over! O wins!")
+        sys.exit()
+    # detects a win on vertical 2,5,8
+    if board[0][2] == 'X' and board[2][2] == 'X' and board[4][2] == 'X':
+        print_board(board)
+        print("Game over! X wins!")
+        sys.exit()
+    if board[0][2] == 'O' and board[2][2] == 'O' and board[4][2] == 'O':
+        print_board(board)
+        print("Game over! O wins!")
+        sys.exit()
+    # detects a win on vertical 2,5,8
+    if board[0][4] == 'X' and board[2][4] == 'X' and board[4][4] == 'X':
+        print_board(board)
+        print("Game over! X wins!")
+        sys.exit()
+    if board[0][4] == 'O' and board[2][4] == 'O' and board[4][4] == 'O':
+        print_board(board)
+        print("Game over! O wins!")
+        sys.exit()
+
+    # HORIZONTAL AND TOTAL PLAYS WIN CHECKER
     # Number of total spaces played
     count = 0
     for row in board:
@@ -100,23 +144,32 @@ def win_checker(board):
         for char in row:
 
             # Steps to perform if character is an X
+            # if count reaches 9, game is over.
+            # if either row_count reaches 3, game is over
             if char == 'X':
                 count = count + 1
                 row_countX = row_countX + 1
                 if row_countX == 3:
+                    print_board(board)
                     print('Game Over! X Wins')
                     sys.exit()
+
             # Steps to perform if character is an O
+            # if count reaches 9, game is over.
+            # if either row_count reaches 3, game is over
             if char == 'O':
                 count = count + 1
                 row_countO = row_countO + 1
                 if row_countO == 3:
                     print('Game Over! O Wins')
                     sys.exit()
-    # Checks if board has 9 X+O's
+
+    # TOTAL MOVE COUNT CHECKER
+    # if count reaches 9, game is over.
     if count == 9:
         print_board(board)
-        print('Game Over! Congratulations!')
+        print("Game Over! It's a Draw!\n")
+
         sys.exit()
 
 
